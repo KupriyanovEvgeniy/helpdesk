01a85667c02e97aeb70261c63bddbb96007d6f8a
Abdulov <zyborevdima999@gmail.com>
2025-10-28 00:02:40 +0500

    style done

--- src/main/java/com/company/helpdesk/view/calendar/Calendar.java
+++ src/main/java/com/company/helpdesk/view/calendar/Calendar.java
@@ -1,14 +1,76 @@
 package com.company.helpdesk.view.calendar;
 
-
 import com.company.helpdesk.view.main.MainView;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Section;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.StandardView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
 
 @Route(value = "calendar", layout = MainView.class)
-@ViewController(id = "Calendar")
-@ViewDescriptor(path = "calendar.xml")
+@ViewController("Calendar")
+@ViewDescriptor("calendar.xml")
+
 public class Calendar extends StandardView {
+    @ViewComponent
+    private Section mainSection;
+    @ViewComponent
+    private Button buttonRight;
+    @ViewComponent
+    private Button buttonLeft;
+
+    @ViewComponent
+    private H1 yearPanel;
+    @ViewComponent
+    private H3 monthLabel;
+    @Autowired
+    private UiComponents uiComponents;
+    public GetDate my_data = new GetDate();
+    @Subscribe
+    public void onInit(final InitEvent event){
+        my_data.getCurrentDate();
+        monthLabel.setText(my_data.getMonthName());
+        yearPanel.setText(my_data.getYearName());
+        for(int i = 0; i < my_data.numbeOfRows(); i++){
+            Div calendar_rows = new Div();
+            Collection<Component> buttons_data = my_data.getButtons(uiComponents);
+            calendar_rows.add(buttons_data);
+            mainSection.add(calendar_rows);
+        }
+    }
+    @Subscribe(id = "buttonRight", subject = "clickListener")
+    public void onButtonRightClick(final ClickEvent<JmixButton> event) {
+        my_data.moveRigth();
+        monthLabel.setText(my_data.getMonthName());
+        yearPanel.setText(my_data.getYearName());
+        mainSection.removeAll();
+        for(int i = 0; i < my_data.numbeOfRows(); i++){
+            Div calendar_rows = new Div();
+            Collection<Component> buttons_data = my_data.getButtons(uiComponents);
+            calendar_rows.add(buttons_data);
+            mainSection.add(calendar_rows);
+        }
+    }
+    @Subscribe(id = "buttonLeft", subject = "clickListener")
+    public void onButtonLeftClick(final ClickEvent<JmixButton> event) {
+        my_data.moveLeft();
+        monthLabel.setText(my_data.getMonthName());
+        yearPanel.setText(my_data.getYearName());
+        mainSection.removeAll();
+        for(int i = 0; i < my_data.numbeOfRows(); i++){
+            Div calendar_rows = new Div();
+            Collection<Component> buttons_data = my_data.getButtons(uiComponents);
+            calendar_rows.add(buttons_data);
+            mainSection.add(calendar_rows);
+        }
+    }
 }
--- src/main/java/com/company/helpdesk/view/calendar/GetDate.java
+++ src/main/java/com/company/helpdesk/view/calendar/GetDate.java
@@ -1,60 +1,107 @@
 package com.company.helpdesk.view.calendar;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Section;
 import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.kit.component.button.JmixButton;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class GetDate {
-    public LocalDate date = LocalDate.now();
-    public LocalDate lastDay = date.withDayOfMonth(date.lengthOfMonth());
-    public String[] month_lenght_str = lastDay.toString().split("-");
-    public int month_lenght_int = Integer.parseInt(month_lenght_str[2]);
-    public int monthDay;
-    public int dayOfWeek;
+    public LocalDate date;
+    public Integer monthLength;
     public int value = 0;
-    public String[] dates = new String[month_lenght_int];
-    public void get_all_month(){
-        for(int i=1; i<=month_lenght_int; i++){
-            monthDay = date.withDayOfMonth(i).getDayOfMonth();
-            dayOfWeek = date.withDayOfMonth(i).getDayOfWeek().getValue();
-            dates[i-1] = monthDay + "-" + dayOfWeek;
+    public String[] monthDates;
+    public void resetToZeroValue(){
+        value = 0;
+    }
+    public Integer numbeOfRows(){
+        int rowCount;
+        String first_date = date.withDayOfMonth(1).getDayOfMonth() + "-" + date.withDayOfMonth(1).getDayOfWeek().getValue();
+        String month = date.getMonth().toString();
+        int monthLength = date.lengthOfMonth();
+        System.out.println(month);
+        if((first_date.equals("1-6")&&monthLength==31)||(first_date.equals("1-7")&&!month.equals("FEBRUARY"))){
+            rowCount = 6;
         }
+        else{
+            rowCount = 5;
+        }
+        return rowCount;
+    }
+    public void getCurrentDate(){
+        date = LocalDate.now();
+        monthLength = date.lengthOfMonth();
+        monthDates = getMonthDays();
+    }
+    public String[] getMonthDays(){
+        String[] dates = new String[monthLength];
+        for(int i=1; i<=monthLength; i++){
+            dates[i-1] = date.withDayOfMonth(i).getDayOfMonth() + "-" + date.withDayOfMonth(i).getDayOfWeek().getValue();
+        }
+        return dates;
     }
     public Collection<Component> getButtons(UiComponents uiComponent){
-        get_all_month();
         Collection<Component> list = new ArrayList<>();
-        System.out.println("HIGIT");
         for (int i = 1; i <= 7; i++) {//не трогай, это дни недели, они статичны
-            if(value < dates.length){
-                int header = Integer.valueOf(dates[value].split("-")[1]);
+            if(value < monthDates.length){
+                int header = Integer.valueOf(monthDates[value].split("-")[1]);
                 if (i == header) {
                     JmixButton button = uiComponent.create(JmixButton.class);
-                    button.setText(dates[value].split("-")[0]);
+                    button.setText(monthDates[value].split("-")[0]);
+                    button.addClassName("calendar-buttons");
                     list.add(button);
                     value++;
                 }
                 else {
                     JmixButton button = uiComponent.create(JmixButton.class);
                     button.setText("-");
+                    button.addClassName("calendar-buttons");
                     list.add(button);
                 }
             }
             else{
                 JmixButton button = uiComponent.create(JmixButton.class);
                 button.setText("-");
+                button.addClassName("calendar-buttons");
                 list.add(button);
             }
         }
         return list;
+    }
+    public String getMonthName(){
+        String month = date.getMonth().toString();
+        switch (month){
+            case "JANUARY":month = "Январь";break;
+            case "FEBRUARY":month = "Февраль";break;
+            case "MARCH":month = "Март";break;
+            case "APRIL":month = "Апрель";break;
+            case "MAY":month = "Май";break;
+            case "JUNE":month = "Июнь";break;
+            case "JULY":month = "Июль";break;
+            case "AUGUST":month = "Август";break;
+            case "SEPTEMBER":month = "Сентябрь";break;
+            case "OCTOBER":month = "Октябрь";break;
+            case "NOVEMBER":month = "Ноябрь";break;
+            case "DECEMBER":month = "Декабрь";break;
+        }
+        return month;
+    }
+    public String getYearName(){
+        String year = Integer.toString(date.getYear());
+        return year;
+    }
+    public void moveLeft(){
+        resetToZeroValue();
+        date = date.minusMonths(1);
+        monthLength = date.lengthOfMonth();
+        monthDates = getMonthDays();
+    }
+    public void moveRigth(){
+        resetToZeroValue();
+        date = date.plusMonths(1);
+        monthLength = date.lengthOfMonth();
+        monthDates = getMonthDays();
     }
 }
